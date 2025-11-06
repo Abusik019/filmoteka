e66e14376d31d8b5959126c921fcb717529c53f0
Абужапар Гасанбеков <abusikgasanbekov@MacBook-Air-Abuzapar.local>
2025-11-06 16:30:26 +0300

    build: update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     reactStrictMode: true,
+    images: {
+        domains: ["image.openmoviedb.com"],
+    },
 };
-
 export default nextConfig;
